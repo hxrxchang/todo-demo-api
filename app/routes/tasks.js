4fa2219d32bf84609ff803b7ea0b6c5bcac7ff7d
hxrxchang <m.y.y.0326.cg@gmail.com>
2018-04-28 18:28:38 +0900

    0428ひとまず

--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../../models');
 
-router.get('/test', (req, res) => {
+router.post('/test', (req, res) => {
   console.log('qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq');
-  console.log(req.query);
-  res.json({
-    message: 'this is tasks api',
+  console.log(req.body);
+  let title = req.boby.todoTitle;
+  let description = req.body.todoDetail;
+  let userId = req.body.userId;
+  let params = {
+    user_id: userId,
+    title,
+    description,
+    is_completed: false,
+    is_deleted: false
+  };
+
+  createTask(params)
+  .then((response) => {
+    console.log(response);
+    res.json({
+      message: 'this is tasks api',
+    });
   });
+  // .catch((err) => {
+  //   console.log(err);
+  //   res.json({
+
+  //   });
+  // });
+
+  // res.json({
+  //   message: 'this is tasks api',
+  // });
+
+  async function createTask(params) {
+    return await db.Task.create(params);
+  }
 });
 
 module.exports = router;
--- models/task.js
+++ models/task.js
@@ -1,12 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Task = sequelize.define('Task', {
+    user_id: DataTypes.INTEGER,
     title: DataTypes.STRING,
     description: DataTypes.STRING,
-    is_completed: DataTypes.BOOLEAN
+    is_completed: DataTypes.BOOLEAN,
+    is_deleted: DataTypes.BOOLEAN
   }, {});
   Task.associate = function(models) {
-    // associations can be defined here
   };
   return Task;
 };
