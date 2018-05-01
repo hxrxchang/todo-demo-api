ddc38c3dcfb1f9b6089eb3cc242a0693e1d942b5
hxrxchang <m.y.y.0326.cg@gmail.com>
2018-05-01 11:31:14 +0900

    make users-controller iikanji

--- app/routes/users.js
+++ app/routes/users.js
@@ -3,41 +3,73 @@ const router = express.Router();
 const db = require('../../models');
 
 router.post('/login', (req, res) => {
-  console.log('1111111111111111111111111');
-  console.log(req.body);
-  res.json({
-    message: 'json from users login controller'
-  });
-});
-
-router.post('/sign-up', (req, res) => {
   let userName = req.body.userName;
   let password = req.body.password;
   let condition = {
     where: {
       name: userName,
-      password,
-    },
+      password
+    }
   };
 
   getUserData(condition)
   .then((response) => {
     console.log(response);
     res.json({
-      message: 'json from users sign-up controller'
+      message: 'success getting user',
+      content: response
+    });
+  })
+  .catch((error) =>{
+    res.json({
+      message: 'fail getting user',
+      content: err
+    });
+  });
+});
+
+router.post('/sign-up', (req, res) => {
+  let userName = req.body.userName;
+  let password = req.body.password;
+
+  createUserData(userName, password)
+  .then((response) => {
+    res.json({
+      message: 'success creating user',
+      content: response
     });
   })
   .catch((error) => {
-    console.log(error);
-    console.log('ここまできてるよ');
     res.json({
-      message: error
+      message: 'fail creating user',
+      content: error
     });
   });
-
-  async function getUserData(condition) {
-    return await db.User.findAll(condition);
-  }
 });
+
+async function createUserData(userName, password) {
+  let condition = {
+    where: {
+      name: userName,
+      password
+    }
+  };
+
+  let userData = await getUserData(condition);
+
+  if (!userData.length) {
+    let params = {
+      name: userName,
+      password
+    };
+    return await db.User.create(params);
+  } else {
+    return null;
+  }
+}
+
+async function getUserData(condition) {
+  return await db.User.findAll(condition);
+}
 
 module.exports = router;
