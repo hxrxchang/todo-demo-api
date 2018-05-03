e572630d3b8f5de4973f40238cc92d3929dec2be
hxrxchang <m.y.y.0326.cg@gmail.com>
2018-05-03 20:36:37 +0900

    完了済みタスクと削除ずみタスクを取得できるようにコンディションを変更

--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -4,12 +4,24 @@ const db = require('../../models');
 
 router.post('/', (req, res) => {
   let userId = req.body.userId;
+  let requestCompletedTask = req.query.requestCompletedTask;
+  let requestDeletedItem = req.body.requestDeletedItem;
   let condition = {
     where: {
       user_id: userId,
+      is_completed: false,
       is_deleted: false
     }
   };
+
+  if (requestCompletedTask) {
+    condition.where.is_completed = true;
+  }
+
+  if (requestDeletedItem) {
+    condition.where.is_deleted = true;
+  }
+
   db.Task.findAll(condition)
   .then((response) => {
     res.json({
