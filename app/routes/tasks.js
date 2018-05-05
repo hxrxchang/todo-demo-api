8aad3acb740b8f746b6f54cde796d8707c6368f2
hxrxchang <m.y.y.0326.cg@gmail.com>
2018-05-06 04:12:52 +0900

    パラメーター変更の対応

--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -4,23 +4,12 @@ const db = require('../../models');
 
 router.post('/', (req, res) => {
   let userId = req.body.userId;
-  let requestCompletedTask = req.body.requestCompletedTask;
-  let requestDeletedTask = req.body.requestDeletedTask;
   let condition = {
     where: {
       user_id: userId,
-      is_completed: false,
       is_deleted: false,
     }
   };
-
-  if (requestCompletedTask) {
-    condition.where.is_completed = true;
-  }
-
-  if (requestDeletedTask) {
-    condition.where.is_deleted = true;
-  }
 
   db.Task.findAll(condition)
   .then((response) => {
