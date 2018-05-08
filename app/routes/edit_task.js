4041220e88cc06d989dd1347e2489f99f9fdec15
hxrxchang <m.y.y.0326.cg@gmail.com>
2018-05-08 17:49:22 +0900

    デッドラインを更新できるように

--- app/routes/edit_task.js
+++ app/routes/edit_task.js
@@ -4,14 +4,16 @@ const db = require('../../models');
 
 router.post('/', (req, res) => {
   let taskId = req.body.taskId;
-  if (!taskId) {
+  if (!taskId && title) {
     throw new Error();
   }
   let title = req.body.newTitle;
   let description = req.body.newDetail;
+  let deadline = req.body.newDeadline;
   let params = {};
   if (title) params.title = title;
   if (description) params.description = description;
+  if (deadline) params.deadline = deadline;
 
   let condition = {
     where: { id: taskId }
